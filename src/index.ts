4eb341a34364bf061700c86cfb70bf3fef3ad513
sinedied <noda@free.fr>
2024-05-06 18:33:17 +0200

    chore: update to latest openai

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import process from 'node:process';
 import { AzureOpenAI } from 'openai';
-import { DefaultAzureCredential } from '@azure/identity';
+import { DefaultAzureCredential, getBearerTokenProvider } from '@azure/identity';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const credential = new DefaultAzureCredential();
-
-// This should be run in azureADTokenProvider delegate function,
-// but currently it's not possible to use async functions there.
-const { token } = await credential.getToken('https://cognitiveservices.azure.com/.default');
-
 const openai = new AzureOpenAI({
-  azureADTokenProvider: () => token,
+  azureADTokenProvider: getBearerTokenProvider(credential, 'https://cognitiveservices.azure.com/.default'),
 });
 
 const result = await openai.chat.completions.create({
